1175eba87b5589187dc7193ae4bef1236b6813aa
Neophytos Antoniou <NeophytosAntoniou1993@gmail.com>
2026-08-22 14:32:19 +0300

    Create Javascript DOM Elements

---
+++ script.js
@@ -0,0 +1,14 @@
+// DOM ELEMENTS
+const addApplicationBtn = document.getElementById("add-application-btn");
+const applicationModal = document.getElementById("application-modal");
+const cancelApplicationBtn = document.getElementById("cancel-application-btn");
+const applicationForm = document.getElementById("application-form");
+
+// Form fields
+const companyInput = document.getElementById("company");
+const positionInput = document.getElementById("position");
+const locationInput = document.getElementById("location");
+const salaryInput = document.getElementById("salary");
+const dateInput = document.getElementById("application-date");
+const statusSelect = document.getElementById("application-status");
+const notesInput = document.getElementById("notes");
