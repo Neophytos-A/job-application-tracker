1c67d4fbfda3ddedb9976bd553cf0cd222d30d7c
Neophytos Antoniou <NeophytosAntoniou1993@gmail.com>
2026-08-24 16:53:14 +0300

    Create variables and object that represent a job application

--- script.js
+++ script.js
@@ -13,10 +13,46 @@ const dateInput = document.getElementById("application-date");
 const statusSelect = document.getElementById("application-status");
 const notesInput = document.getElementById("notes");
 
+// Array that holds all aplication objects
+let applications = [];
+
+// Open Modal
 addApplicationBtn.addEventListener("click", function () {
   applicationModal.classList.add("active");
 });
 
+// Close Modal
 cancelApplicationBtn.addEventListener("click", function () {
   applicationModal.classList.remove("active");
+});
+
+// Prevents the browser from refreshing the page when the form is submitted
+applicationForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  // Read the values from inputs
+  const company = companyInput.value.trim();
+  const position = positionInput.value.trim();
+  const location = locationInput.value.trim();
+  const salary = salaryInput.value;
+  const applicationDate = dateInput.value;
+  const status = statusSelect.value;
+  const notes = notesInput.value.trim();
+
+  // Object that represents a job application
+  const newApplication = {
+    id: Date.now(),
+    company: company,
+    position: position,
+    location: location,
+    salary: salary,
+    applicationDate: applicationDate,
+    status: status,
+    notes: notes,
+  };
+
+  applications.push(newApplication);
+
+  console.log(newApplication);
+  console.log(applications);
 });
